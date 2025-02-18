d6b7c707c5c26dd2dcf90643585533810c892dd9
GabrielPlus <gabrielpius0311@gmail.com>
2025-02-18 09:33:04 +0300

    prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   images: {
-    domains: [
-        "res.cloudinary.com"
-    ]
-   }
-};
+    images: {
+      domains: ["res.cloudinary.com"],
+    },
+    experimental: {
+      runtime: "nodejs",
+    },
+  };
+  
+  export default nextConfig;
+  
 
-export default nextConfig;
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//    images: {
+//     domains: [
+//         "res.cloudinary.com"
+//     ]
+//    }
+// };
+
+// export default nextConfig;
