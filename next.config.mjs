58ee87e4ab1f9dbfc0aa55e6f8033df08df89b9a
pkoooo <pkoooo@users.noreply.github.com>
2025-11-21 14:16:35 +0300

    adddd

--- next.config.mjs
+++ next.config.mjs
@@ -78,7 +78,7 @@ const nextConfig = {
                     // CORS headers
                     {
                         key: "Access-Control-Allow-Origin",
-                        value: process.env.FRONTEND_STORE_URL || "https://rwaya.vercel.app",
+                        value: process.env.FRONTEND_STORE_URL || "https://bagenvy.vercel.app/",
                     },
                     {
                         key: "Access-Control-Allow-Methods",
